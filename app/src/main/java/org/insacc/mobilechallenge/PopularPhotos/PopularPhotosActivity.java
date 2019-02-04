15352afad51822a4c54b3d30280efa7179ae9145
Can Undeger <canundeger@gmail.com>
2019-02-04 00:31:59 -0500

    Use placeholder for photo grid

--- app/src/main/java/org/insacc/mobilechallenge/PopularPhotos/PopularPhotosActivity.java
+++ app/src/main/java/org/insacc/mobilechallenge/PopularPhotos/PopularPhotosActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 
 import android.view.View;
-import android.widget.Toast;
 
 import com.fivehundredpx.greedolayout.GreedoLayoutManager;
 
